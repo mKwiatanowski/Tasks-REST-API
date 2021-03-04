6919899f8fca58fe2cff2da58103fe17564482a2
Mateusz Kwiatanowski <mateusz.kwiatanowski@gmail.com>
2021-03-04 21:31:31 +0100

    19.3 Delete

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -5,6 +5,7 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,23 +26,35 @@ public class TaskController {
     }
 
     @GetMapping(value = "getTask")
-    public TaskDto getTask(Long taskId){
-        return new TaskDto(1L, "Test title", "test content");
+    public TaskDto getTask(@RequestParam Long taskId) throws TaskNotFoundException {
+        return taskMapper.mapToTaskDto(service.getTask(taskId).orElseThrow(TaskNotFoundException::new));
+
     }
 
     @DeleteMapping(value = "deleteTask")
-    public void deleteTask(Long taskId){
+    public void deleteTask(@RequestParam Long taskId) throws TaskNotFoundException {
+        if (service.getTask(taskId).isPresent()){
+            service.deleteTask(taskId);
+        } else {
+            throw new TaskNotFoundException();
+        }
 
     }
 
     @PutMapping(value = "updateTask")
-    public TaskDto updateTask(TaskDto taskDto){
-        return new TaskDto(1L, "Edited test title", "Test content");
+    public TaskDto updateTask(@RequestBody TaskDto taskDto){
+        Task task = taskMapper.mapToTask(taskDto);
+        Task saveTask = service.saveTask(task);
+        return taskMapper.mapToTaskDto(saveTask);
+
     }
 
-    @PostMapping(value = "createTask")
+    @PostMapping(value = "createTask", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createTask(TaskDto taskDto){
-
+        Task task = taskMapper.mapToTask(taskDto);
+        service.saveTask(task);
     }
+
+
 
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,9 +20,21 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTask(Long id){
-        return repository.findById(id).orElse(null);
+    public Optional<Task> getTask(final Long id){
+        return repository.findById(id);
     }
+
+    public Task saveTask(final Task task){
+        return repository.save(task);
+    }
+
+    public void deleteTask (Long id){
+        repository.deleteById(id);
+    }
+
+
+
+
 
 
 
