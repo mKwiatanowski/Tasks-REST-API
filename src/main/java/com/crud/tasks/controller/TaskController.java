e971c117e7fab7e002edcca28757206be766b23b
Mateusz Kwiatanowski <mateusz.kwiatanowski@gmail.com>
2021-06-09 20:47:06 +0200

    fix wrong URL

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/task/")
+@RequestMapping("/v1/task")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class TaskController {
